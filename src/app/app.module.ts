f79a0b997008d423860972c57a3f5671e2f05bd9
Quentin Blt <gastbob40@gmail.com>
2019-04-21 19:16:07 +0200

    Add of coockie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { ArticleComponent } from './components/article/article.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CookieLawModule } from 'angular2-cookie-law';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ArticleComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ArticleComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CookieLawModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
